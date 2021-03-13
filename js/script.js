8391d1b6e1a2096eeb3cd254a6efa8d79c531162
Fajar M <habibimochfajar.ah@gmail.com>
2021-03-13 09:05:58 +0700

    Mengubah Scrollbar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
+//Parallax
+var scene = document.getElementById("scene");
+var parallax = new Parallax(scene);
 
+//Contact Form
+const scriptURL = "https://script.google.com/macros/s/AKfycbxCE7rRqiKL9jIfKbYMjCE5ScTKENBzLOe1WNphmgoFrhG0DIaTo9JrdJCFwyjJn5fSaA/exec";
+const form = document.forms["portfolio-contact"];
+const btnKirim = document.querySelector(".btn-kirim");
+const btnLoading = document.querySelector(".btn-loading");
+const myAlert = document.querySelector(".alert");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // Menghilangkan button kirim, Menampilkan Button loading
+  btnLoading.classList.toggle("d-none");
+  btnKirim.classList.toggle("d-none");
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      console.log("Success!", response);
+      // Menghilangkan button loading, Menampilkan Button kirim
+      btnLoading.classList.toggle("d-none");
+      btnKirim.classList.toggle("d-none");
+      //menampilkan alert
+      myAlert.classList.toggle("d-none");
+      //reset form
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
